7ce8a4cab94f52b3c904195eb8b4a9e57f38a847
Akif Oezkan <akif.oezkan@fau.de>
2019-01-31 16:51:17 +0100

    node_gen is merged to hipacc_gen

--- hipaVX/hipacc_gen/hipacc_gen.hpp
+++ hipaVX/hipacc_gen/hipacc_gen.hpp
@@ -1,10 +1,32 @@
+#include "../ast.hpp"
 #include "../gen_template.hpp"
-#include "config_reader.hpp"
 #include "../kernels/domVX_kernels.hpp"
-#include "node_gen.hpp"
+
+#include "ast_gen.hpp"
+#include "config_reader.hpp"
+
+#include <iostream>
+#include <vector>
+#include <string>
+
 
 #pragma once
 
 using std::string;
+
+static string hipaVX_folder = "hipaVX/hipacc_gen";
+
+static std::map<vx_df_image, string> VX_DF_IMAGE_to_hipacc = {
+    {VX_DF_IMAGE_U8, "uchar"},
+    {VX_DF_IMAGE_S16, "short"},
+    {VX_DF_IMAGE_S32, "int"},
+    {VX_DF_IMAGE_U32, "uint"},
+    {VX_DF_IMAGE_RGBX, "uchar4"},
+    {VX_TYPE_FLOAT32, "float"}, //Not really a vx_df_image type
+    {VX_TYPE_INT8, "uchar"}, //Not really a vx_df_image type
+    {VX_TYPE_INT32, "int"} //Not really a vx_df_image type
+};
+
+string generate_image_name(HipaVX::Image *image);
 
 void process_graph(HipaVX::Graph *graph);
