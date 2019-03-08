99480fe6cb9879a390baf152898ab5babe50d589
Akif Oezkan <akif.oezkan@fau.de>
2019-03-08 18:56:40 +0100

    domVX_types: Object

--- hipaVX/domVX_types.cpp
+++ hipaVX/domVX_types.cpp
@@ -1,82 +1,48 @@
-#include "../VX/vx.h"
 #include "domVX_types.hpp"
+#include "../VX/vx.h"
 
 #include <iostream>
 
-
-function_ast::Datatype convert_type(vx_df_image type)
-{
-	switch(type)
-	{
-	case VX_DF_IMAGE_U8:
-		return function_ast::Datatype::UINT8;
-	case VX_DF_IMAGE_S16:
-		return function_ast::Datatype::INT16;
-	case VX_DF_IMAGE_U16:
-		return function_ast::Datatype::UINT16;
-	case VX_DF_IMAGE_S32:
-		return function_ast::Datatype::INT32;
-	case VX_DF_IMAGE_U32:
-		return function_ast::Datatype::UINT32;
-	case VX_TYPE_FLOAT32:
-		return function_ast::Datatype::FLOAT;
-	}
-	throw std::runtime_error("Unknown type in function_ast::Datatype convert");
+function_ast::Datatype convert_type(vx_df_image type) {
+  switch (type) {
+    case VX_DF_IMAGE_U8:
+      return function_ast::Datatype::UINT8;
+    case VX_DF_IMAGE_S16:
+      return function_ast::Datatype::INT16;
+    case VX_DF_IMAGE_U16:
+      return function_ast::Datatype::UINT16;
+    case VX_DF_IMAGE_S32:
+      return function_ast::Datatype::INT32;
+    case VX_DF_IMAGE_U32:
+      return function_ast::Datatype::UINT32;
+    case VX_TYPE_FLOAT32:
+      return function_ast::Datatype::FLOAT;
+  }
+  throw std::runtime_error("Unknown type in function_ast::Datatype convert");
 }
 
+namespace HipaVX {
+VertexTask set_task_from_type(HipaVX::ObjectType type) {
+  switch (type) {
+    case VX_TYPE_IMAGE:
+    case VX_TYPE_SCALAR:
+      return VertexTask::Buffer;
+    case VX_TYPE_NODE:
+      return VertexTask::Computation;
+    default:
+      throw std::runtime_error("Unsupported vx_type_e in set_task");
+  }
+}
 
-namespace HipaVX
-{
-
-// Object
 int Object::next_id = 0;
 
-Object::Object()
-    :my_id(next_id++)
-{
-}
-
-// Scalar
-Scalar::Scalar(vx_type_e t, const void *ptr)
-    :data_type(t)
-{
-    type = VX_TYPE_SCALAR;
-    switch(data_type)
-    {
-    case VX_TYPE_INT32:
-        i32 = *((vx_int32*) ptr);
-        break;
-    case VX_TYPE_FLOAT32:
-        f32 = *((vx_float32*) ptr);
-        break;
-    default:
-        throw std::runtime_error("Unsupported vx_type_e in Scalar constructor");
-    }
-}
-
-// Image
-Image::Image(vx_uint32 width, vx_uint32 height, vx_df_image color)
-    :w(width), h(height), col(color)
-{
-    type = VX_TYPE_IMAGE;
-}
-
-// Array
-Array::Array(vx_enum item_type, vx_size cap, vx_size rows)
-    :Image(rows, cap, VX_DF_IMAGE_S32), type(item_type), capacity(cap)
-{
-    type = VX_TYPE_ARRAY;
-}
-
 // Graph
-void Graph::build()
-{
-    for (auto& node: graph)
-    {
-        node->build();
-    }
+void Graph::build() {
+  for (auto& node : graph) {
+    node->build();
+  }
 
-    built = true;
+  built = true;
 }
 
-}
+}  // namespace HipaVX
--- hipaVX/domVX_types.hpp
+++ hipaVX/domVX_types.hpp
@@ -1,211 +1,231 @@
 #pragma once
 
+#include "../VX/vx.h"
 #include "../VX/vx_types.h"
 #include "ast.hpp"
 
-#include <vector>
 #include <string>
+#include <vector>
 
 function_ast::Datatype convert_type(vx_df_image type);
 
-namespace HipaVX
-{
-class Object
-{
-	static int next_id;
-public:
-	const int my_id;
-	Object();
+// making implementation names independent from the standard
+namespace HipaVX {
+using ObjectType = vx_type_e;
+}  // namespace HipaVX
 
-	vx_type_e type;
+namespace HipaVX {
+enum class VertexTask { Computation, Buffer, None };
+
+VertexTask set_task_from_type(HipaVX::ObjectType type);
+
+class Object {
+ public:
+  Object() : my_id(next_id++){};
+
+  bool is_virtual() { return virt; };
+
+  VertexTask get_task() { return task; }
+
+  void set_task(VertexTask _task) { task = _task; }
+
+  void set_task() { set_task_from_type(type); }
+
+  void set_name(std::string name = "Object") {
+    name = name + std::to_string(my_id);
+  }
+
+ public:
+  // TODO: make these protected
+  const int my_id;
+
+  std::string name;
+
+  ObjectType type;
+
+ protected:
+  static int next_id;
+
+  VertexTask task;
+
+  bool virt;
 };
 
-class Scalar: public Object
-{
-public:
-	Scalar(vx_type_e t, const void *ptr);
+class Scalar : public Object {
+ public:
+  Scalar(ObjectType t, const void *ptr) : data_type(t) {
+    type = VX_TYPE_SCALAR;
+    switch (data_type) {
+      case VX_TYPE_INT32:
+        i32 = *((vx_int32 *)ptr);
+        break;
+      case VX_TYPE_FLOAT32:
+        f32 = *((vx_float32 *)ptr);
+        break;
+      default:
+        throw std::runtime_error("Unsupported vx_type_e in Scalar constructor");
+    }
+  }
 
-	vx_type_e data_type;
-	union
-	{
-		vx_char c;
-		vx_int8 i8;
-		vx_uint8 ui8;
-		vx_int16 i16;
-		vx_uint16 ui16;
-		vx_int32 i32;
-		vx_uint32 ui32;
-		vx_int64 i64;
-		vx_uint64 ui64;
-		vx_float32 f32;
-		vx_float64 f64;
-		vx_enum e;
-		vx_size s;
-		vx_df_image df_image;
-		vx_bool b;
-	};
-
+  ObjectType data_type;
+  union {
+    vx_char c;
+    vx_int8 i8;
+    vx_uint8 ui8;
+    vx_int16 i16;
+    vx_uint16 ui16;
+    vx_int32 i32;
+    vx_uint32 ui32;
+    vx_int64 i64;
+    vx_uint64 ui64;
+    vx_float32 f32;
+    vx_float64 f64;
+    vx_enum e;
+    vx_size s;
+    vx_df_image df_image;
+    vx_bool b;
+  };
 };
 
-class Image: public Object
-{
-public:
-	Image(vx_uint32 width, vx_uint32 height, vx_df_image color);
-	Image() {};
-	virtual ~Image() = default;
-	vx_uint32 w=0, h=0;
-	vx_df_image col;
+class Image : public Object {
+ public:
+  Image(){};
+
+  Image(vx_uint32 width, vx_uint32 height, vx_df_image color) :
+      w(width), h(height), col(color) {
+    type = VX_TYPE_IMAGE;
+  }
+
+  virtual ~Image() = default;
+  vx_uint32 w = 0, h = 0;
+  vx_df_image col;
 };
 
-class Array: public Image
-{
-public:
-	Array(vx_enum item_type, vx_size cap, vx_size rows);
-	virtual ~Array() = default;
-	vx_enum type;
-	vx_size capacity;
+class Array : public Image {
+ public:
+  Array(vx_enum item_type, vx_size cap, vx_size rows) : 
+      Image(rows, cap, VX_DF_IMAGE_S32), 
+      type(item_type), capacity(cap) {
+    type = VX_TYPE_ARRAY;
+  }
+
+  virtual ~Array() = default;
+  vx_enum type;
+  vx_size capacity;
 };
 
-class Node: public Object
-{
-protected:
-	std::string node_name = "nodename not set";
-public:
-	Node()
-	{
-		type = VX_TYPE_NODE;
-		kernel = std::make_shared<function_ast::ForEveryPixel>();
-	}
-	virtual ~Node() = default;
-	std::string get_name() {return node_name;}
+class Node : public Object {
+ protected:
+  std::string node_name = "nodename not set";
 
-    std::vector<Node*> subnodes;
-    std::vector<Object*> inputs;
-    std::vector<Object*> outputs;
+ public:
+  Node() {
+    type = VX_TYPE_NODE;
+    kernel = std::make_shared<function_ast::ForEveryPixel>();
+  }
+  virtual ~Node() = default;
+  std::string get_name() { return node_name; }
 
-    vx_border_e border_mode = VX_BORDER_UNDEFINED;
-	std::shared_ptr<function_ast::ForEveryPixel> kernel;
+  std::vector<Node *> subnodes;
+  std::vector<Object *> inputs;
+  std::vector<Object *> outputs;
 
-    virtual void build(){}
+  vx_border_e border_mode = VX_BORDER_UNDEFINED;
+  std::shared_ptr<function_ast::ForEveryPixel> kernel;
+
+  virtual void build() {}
 };
 
-class Graph: public Object
-{
-public:
-	std::vector<Node*> graph;
-	bool built = false;
+class Graph : public Object {
+ public:
+  std::vector<Node *> graph;
+  bool built = false;
 
-	void build();
+  void build();
 };
 
-class VX_Matrix: public Object
-{
-public:
-	VX_Matrix()
-	{
-		type = VX_TYPE_MATRIX;
-	}
+class VX_Matrix : public Object {
+ public:
+  VX_Matrix() { type = VX_TYPE_MATRIX; }
 
-	vx_enum data_type;
-	vx_size rows;
-	vx_size columns;
-	std::vector<u_char> mat;
+  vx_enum data_type;
+  vx_size rows;
+  vx_size columns;
+  std::vector<u_char> mat;
 };
 
-class Threshold: public Object
-{
-public:
-	Threshold()
-	{
-		type = VX_TYPE_THRESHOLD;
-	}
-	vx_threshold_type_e threshold_type;
+class Threshold : public Object {
+ public:
+  Threshold() { type = VX_TYPE_THRESHOLD; }
+  vx_threshold_type_e threshold_type;
 
-	vx_int32 value;
-	vx_int32 lower;
-	vx_int32 upper;
+  vx_int32 value;
+  vx_int32 lower;
+  vx_int32 upper;
 
-	vx_int32 true_value = 255;
-	vx_int32 false_value = 0;
+  vx_int32 true_value = 255;
+  vx_int32 false_value = 0;
 
-	vx_df_image input_format;
-	vx_df_image output_format;
+  vx_df_image input_format;
+  vx_df_image output_format;
 };
 
-class Context: public Object
-{
-public:
-
-	std::vector<Image*> images;
-	std::vector<Graph*> graphs;
+class Context : public Object {
+ public:
+  std::vector<Image *> images;
+  std::vector<Graph *> graphs;
 };
 
-class Convolution: public Object
-{
-public:
-	Convolution()
-	{
-		type = VX_TYPE_CONVOLUTION;
-	}
-	std::vector<vx_int16> coefficients;
-	vx_size  rows;
-	vx_size  columns;
-	vx_uint32 scale;
+class Convolution : public Object {
+ public:
+  Convolution() { type = VX_TYPE_CONVOLUTION; }
+  std::vector<vx_int16> coefficients;
+  vx_size rows;
+  vx_size columns;
+  vx_uint32 scale;
 };
-}
+}  // namespace HipaVX
 
-
-struct _vx_reference
-{
-	HipaVX::Object *o;
+struct _vx_reference {
+  HipaVX::Object *o;
 };
 
-struct _vx_scalar
-{
-	HipaVX::Object *o;
+struct _vx_scalar {
+  HipaVX::Object *o;
 };
 
-struct _vx_kernel
-{
-	HipaVX::Object *o;
+struct _vx_kernel {
+  HipaVX::Object *o;
 };
 
-struct _vx_node
-{
-	HipaVX::Object *o;
+struct _vx_node {
+  HipaVX::Object *o;
 };
 
-struct _vx_graph
-{
-	HipaVX::Object *o;
+struct _vx_graph {
+  HipaVX::Object *o;
 };
 
-struct _vx_context
-{
-	HipaVX::Object *o;
+struct _vx_context {
+  HipaVX::Object *o;
 };
 
-struct _vx_matrix
-{
-	HipaVX::Object *o;
+struct _vx_matrix {
+  HipaVX::Object *o;
 };
 
-struct _vx_threshold
-{
-	HipaVX::Object *o;
+struct _vx_threshold {
+  HipaVX::Object *o;
 };
 
-struct _vx_convolution
-{
-	HipaVX::Object *o;
+struct _vx_convolution {
+  HipaVX::Object *o;
 };
 
-struct _vx_array
-{
-	HipaVX::Object *o;
+struct _vx_array {
+  HipaVX::Object *o;
 };
 
-struct _vx_image
-{
-	HipaVX::Object *o;
+struct _vx_image {
+  HipaVX::Object *o;
 };
