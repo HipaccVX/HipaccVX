3005deed17b4e971c9cde1bdddf6e026bbae89bb
Bur0k <bur0k-git@bur0k.de>
2019-04-07 18:22:38 +0200

    Revert "HipaccGenerator: Added map abstraction support" This will be implemented in another branch This reverts commit 6c0245deaf019e21a611e969b4791170399e4813.

--- hipaVX/hipacc_gen/hipacc_gen.hpp
+++ hipaVX/hipacc_gen/hipacc_gen.hpp
@@ -1,7 +1,6 @@
 #include "../ast.hpp"
 #include "../gen_template.hpp"
 #include "../kernels/domVX_kernels.hpp"
-#include "../abstractions.hpp"
 
 #include "config_reader.hpp"
 
@@ -31,24 +30,8 @@ string generate_image_name(HipaVX::Image *image);
 
 void process_graph(HipaVX::Graph *graph);
 
-class HipaccVisitor: public ASTVisitor<std::string, int>, public AbstractionsVisitor<std::string, int>
+class HipaccVisitor: public ASTVisitor<std::string, int>
 {
-    std::vector<std::string> *pixelaccessor_mapping = nullptr;
-    std::vector<std::string> *windowdescriptor_mapping = nullptr;
-    std::vector<std::string> *maskaccessor_mapping = nullptr;
-
-    std::string accumulator_string = "";
-
-    std::string current_output_pixel_index = "";
-
-    std::map<std::shared_ptr<ast4vx::WindowDescriptor>, std::string> desc_to_name;
-
 public:
-    virtual std::string visit(std::shared_ptr<ast4vx::Node> n, int i = 0) override;
-
-    virtual std::string visit(std::shared_ptr<DomVX::AbstractionNode> n, int i = 0) override;
-
-    std::string setup_outer_loop(std::shared_ptr<DomVX::Map> m);
-    std::string setup_outer_loop(std::shared_ptr<DomVX::LocalOperation> m, const std::vector<HipaVX::Image *> &out);
+    virtual std::string visit(std::shared_ptr<ast4vx::Node> n, int i) override;
 };
-
